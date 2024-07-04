d654f82b3f30245efc43a472f962341b439ea3e5
BloodSweetAn <deyvidjosephg@gmail.com>
2024-07-03 21:58:51 -0500

    Se coloco el PreAuthority

--- src/main/java/pe/edu/upc/demoauthor/controller/CertificationController.java
+++ src/main/java/pe/edu/upc/demoauthor/controller/CertificationController.java
@@ -3,6 +3,7 @@ package pe.edu.upc.demoauthor.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class CertificationController {
     @Autowired
     private ICertification djgcICertification;
 
+    @PreAuthorize("hasAuthority('Programador')")
     @GetMapping("/Quantity")
     public ResponseEntity<?> listCuantityForUser() {
         try {
@@ -27,6 +29,7 @@ public class CertificationController {
         }
     }
 
+    @PreAuthorize("hasAuthority('Reclutador')")
     @GetMapping("/Investment")
     public ResponseEntity<?> listInvestmentForUser() {
         try {
